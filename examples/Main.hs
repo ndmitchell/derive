a81fd9338cab87318c7ef43b05f3deeb896d5e8a
Neil Mitchell <ndmitchell@gmail.com>
2007-03-24 18:45:30 +0000

    Uncomment some examples, commented out by the Functor patch

--- examples/Main.hs
+++ examples/Main.hs
@@ -1,10 +1,10 @@
 import Example
 import Data.Derive.Driver
 
-main = do --f (undefined :: Foo)
-          --f (undefined :: Color)
-          --f (undefined :: Computer)
-          --f (undefined :: Drinks)
+main = do f (undefined :: Foo)
+          f (undefined :: Color)
+          f (undefined :: Computer)
+          f (undefined :: Drinks)
           f (undefined :: FailList A B)
           f (undefined :: State    A B)
     where
