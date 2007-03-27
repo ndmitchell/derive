2a682d3db57537ffed7dd18498bd4f9feeef7235
Stefan O'Rear <stefanor@cox.net>
2007-03-27 00:59:44 +0000

    Factor the list of supported derivations out of the SYB driver

---
+++ Data/Derive/AllDerivers.hs
@@ -0,0 +1,29 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
+-- | This modules contains the common table of derivers; it is
+-- imported by all the driver modules.
+module Data.Derive.AllDerivers
+       (derivable, getDeriver) where
+
+import Data.Derive
+import Data.Maybe
+
+import qualified Data.Derive.Eq
+import qualified Data.Derive.BinaryDefer
+import qualified Data.Derive.Binary
+import qualified Data.Derive.Functor
+
+-- | List of things we are able to derive.
+derivable :: [String]
+derivable = map fst derivers
+
+-- | The main lookup table mapping class names to functions able to
+-- achieve derivation.
+derivers = [("Eq",Data.Derive.Eq.derive)
+           ,("BinaryDefer",Data.Derive.BinaryDefer.derive)
+           ,("Binary",Data.Derive.Binary.derive)
+           ,("Functor",Data.Derive.Functor.derive)
+           ]
+
+-- | Map a class name to a derivation function.
+getDeriver :: String -> (DataDef -> [String])
+getDeriver x = fromMaybe (error $ "Do not know how to derive " ++ x) (lookup x derivers)
--- Data/Derive/Driver.hs
+++ Data/Derive/Driver.hs
@@ -15,27 +15,7 @@ import Data.Generics
 import Data.Derive
 import Data.List
 import Data.Maybe
-
-import qualified Data.Derive.Eq
-import qualified Data.Derive.BinaryDefer
-import qualified Data.Derive.Binary
-import qualified Data.Derive.Functor
-
--- | List of things we are able to derive.
-derivable :: [String]
-derivable = map fst derivers
-
--- | The main lookup table mapping class names to functions able to
--- achieve derivation.
-derivers = [("Eq",Data.Derive.Eq.derive)
-           ,("BinaryDefer",Data.Derive.BinaryDefer.derive)
-           ,("Binary",Data.Derive.Binary.derive)
-           ,("Functor",Data.Derive.Functor.derive)
-           ]
-
--- | Map a class name to a derivation function.
-getDeriver :: String -> (DataDef -> [String])
-getDeriver x = fromMaybe (error $ "Do not know how to derive " ++ x) (lookup x derivers)
+import Data.Derive.AllDerivers
 
 -- | Derive an instance of some class.  This uses the Scrap Your
 -- Boilerplate infrastructure to extract the data type definition; to
@@ -52,7 +32,6 @@ derive s x = putStr $ unlines $ getDeriver s $ fromMaybe (error "Cannot derive f
 -- exactly like 'derive'.
 derives :: (Data a, Typeable a) => String -> a -> IO ()
 derives s x = putStr $ unlines $ concat $ intersperse [""] $ map (getDeriver s) $ deriveMany x
-
 
 -- $arg
 --
