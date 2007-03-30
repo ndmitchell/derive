671d31d1fd92dedb8447d1da7fc71786a4140ab2
Stefan O'Rear <stefanor@cox.net>
2007-03-30 04:25:17 +0000

    Major Eq refactors, trying to make a lib

--- Data/Derive/Eq.hs
+++ Data/Derive/Eq.hs
@@ -9,15 +9,44 @@ eq = Derivation eq' "Eq"
 eq' dat@(DataDef _ _ ctors) = simple_instance ''Eq dat [FunD '(==) (cases ++ [end])]
     where
         cases = map (\ctor -> rule (ctorName ctor) (ctorArity ctor)) ctors
-        end = Clause [WildP, WildP] (NormalB $ ConE 'False) []
+        end = defclause 2 false
 
-rule nam arity = Clause pats (NormalB $ if arity == 0 then ConE 'True else body) []
+rule (CtorDef nam arity _) = clause pats (if arity == 0 then ConE 'True else body)
     where
-        na f c = map (f . mkName . (c:) . show) [1..arity]
-        pats = (map (\c -> ConP (mkName nam) (na VarP c)) "ab")
-        infx op a b = InfixE (Just a) (VarE op) (Just b)
-        body = foldr1 (infx '(&&)) (zipWith (infx '(==)) (na VarE 'a') (na VarE 'b'))
+        na c = map (vr . (c:) . show) [1..arity]
+        pats = map (lK nam . na c) "ab"
+        body = foldr1 (&&:) (zipWith (==:) (na 'a') (na 'b'))
 
+-- | A simple clause, without where or guards
+clause pats body = Clause pats (NormalB body) []
+
+-- | A default clause with N arguments
+defclause num = clause (replicate num WildP)
+
+class Valcon a where
+      lK :: String -> [a] -> a
+      vr :: String -> a
+instance Valcon Exp where
+      lK nm@(x:_) | isLower x = foldl AppE (VarE (mkName nm))
+      lK nm = foldl AppE (ConE (mkName nm))
+      vr = VarE . mkName
+instance Valcon Pat where
+      lK = ConP . mkName
+      vr = VarP . mkName
+
+l0 s     = lK s []
+l1 s a   = lK s [a]
+l2 s a b = lK s [a,b]
+
+-- lifting
+true = l0 "True"
+false = l0 "False"
+
+(==:) = l2 "=="
+(&&:) = l2 "&&"
+
+-- | Build an instance of a class for a data type, using the heuristic
+-- that the type is itself required on all type arguments.
 simple_instance cls (DataDef name arity _) defs = [InstanceD ctx hed defs]
     where
         vars = map (VarT . mkName . ('t':) . show) [1..arity]
