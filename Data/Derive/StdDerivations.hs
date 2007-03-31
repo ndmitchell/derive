26cacd5ce3f08ebad6a1bd85bddcbc89a71bd3f6
Stefan O'Rear <stefanor@cox.net>
2007-03-31 08:01:48 +0000

    Disable functor deriving for now

--- Data/Derive/StdDerivations.hs
+++ Data/Derive/StdDerivations.hs
@@ -6,15 +6,15 @@
 module Data.Derive.StdDerivations
        (derivable,
         module Data.Derive.Eq,
-        module Data.Derive.Functor,
+        --module Data.Derive.Functor,
         module Data.Derive.Binary,
         module Data.Derive.BinaryDefer
        ) where
 
 import Data.Derive.Eq
-import Data.Derive.Functor
+--import Data.Derive.Functor
 import Data.Derive.Binary
 import Data.Derive.BinaryDefer
 
 -- | A default set of standard derivations.
-derivable = [ eq, functor, binary, binarydefer ]
+derivable = [ eq, binary, binarydefer ]
