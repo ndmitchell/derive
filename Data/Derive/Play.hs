4697dad2406baa60555e0722310f467e3459d576
Neil Mitchell <ndmitchell@gmail.com>
2007-04-01 14:06:37 +0000

    Rewrite a lot of the Play code, now getChildren mainly works

--- Data/Derive/Play.hs
+++ Data/Derive/Play.hs
@@ -13,59 +13,58 @@ makePlay :: Derivation
 makePlay = Derivation derive "Play"
 
 
-data Container = None | Item
+data Container = None | Target
                | Container String [Container]
-               | Var String Container
                  deriving (Eq, Show)
 
 
 typeToContainer :: String -> RType -> Container
 typeToContainer active (RType (TypeCon x) xs)
-    | x == active = Item
+    | x == active = Target
     | otherwise = if all (== None) ys then None else Container x ys
         where ys = map (typeToContainer active) xs
 typeToContainer _ _ = None
-
-
-addNames :: Container -> Container
-addNames (Container name xs) = Container name $ evalState (mapM f xs) ['x' : show i | i <- [1..]]
-    where
-        f None = return None
-        f Item = getName >>= \y -> return $ Var y Item
-        f o@(Container "[]" _) = getName >>= \y -> return $ Var y o
-        f x = error $ "Don't know how to derive Play for: " ++ show x
-
-        getName = do (y:ys) <- get; put ys; return y
 
 
 
 derive dat@(DataDef name arity ctors) =
         simple_instance "Play" dat [funN "getChildren" gbody] -- , funN "replaceChildren" rbody]
     where
-        gbody = [gitem (addNames $ Container (ctorName ctor) $ map (typeToContainer name) (ctorTypes ctor))
-                | ctor <- ctors]
+        contain = map (typeToContainer name) . ctorTypes
+        var = vr . ('x':) . show
+        match ctor = sclause [ctp ctor 'x']
+    
+        gbody = [match ctor (gitem $ contain ctor) | ctor <- ctors]
         
-        gitem x = sclause [lhs x] (rhs x) -- f x) (lit (0 :: Integer)) (lit (0 :: Integer)) -- error $ show x -- ctor = error $ show (ctor, map (typeToContainer name) (ctorTypes ctor))
+        gitem :: [Container] -> Exp
+        gitem conts = concat' [AppE (f c) (var i) | (i,c) <- zip [1..] conts]
             where
-                lhs None = WildP
-                lhs (Container x xs) = lK x (map lhs xs)
-                lhs (Var x _) = vr x
+                f None = l1 "const" nil
+                f Target = l0 "id"
+                f (Container "[]" [x]) = l1 "map" (f x)
+                f (Container "," [x,None]) = l2 "." (l0 "fst") (f x)
+                f (Container "," [None,x]) = l2 "." (l0 "snd") (f x)
+                f (Container "," [x,y]) = LamE [l2 "," (vr "t1") (vr "t2")]
+                                          (AppE (f x) (vr "t1") ++: AppE (f y) (vr "t2"))
                 
-                rhs (Container _ xs) = if null res then l0 "[]" else fromEither $ foldr1 f res
-                    where res = concatMap rhss xs
+                -- = l2 "concatMap" [Right $ var i]
+                --f i (Container "[]" [x]) = [Right $ var i]
+                --f i (Container "," [None,Target]) = [Right $ l2 "map" (l0 "snd") (var i)]
                 
-                fromEither (Right x) = x
-                fromEither (Left  x) = l2 ":" x (l0 "[]")
+                f x = error $ "Play.gitem, unhandled case: " ++ show x
                 
-                f (Left  x) y = Right $ l2 ":"  x (fromEither y)
-                f (Right x) y = Right $ l2 "++" x (fromEither y)
                 
+
+{-                
                 -- return Left for an item, Right for a list
                 rhss (Container _ xs) = concatMap rhss xs
                 rhss None = []
-                rhss (Var x Item) = [Left (vr x)]
-                rhss (Var x (Container "[]" [Item])) = [Right (vr x)]
+                rhss (Var x Target) = [Left (vr x)]
+                rhss (Var x (Container "[]" [Target])) = [Right (vr x)]
                 rhss x = error $ "Right hand side of Play not handled, " ++ show x
+-}
+
+
 
 {-        
         
