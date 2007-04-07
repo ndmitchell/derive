41c4dd881356716e5ef2167592742284e1033bbd
Neil Mitchell <ndmitchell@gmail.com>
2007-04-07 10:26:43 +0000

    Delete is, no longer required

--- Data/Derive/Example.hs
+++ Data/Derive/Example.hs
@@ -1,4 +1,4 @@
-module Data.Derive.Example(Data2(..), is, guess) where
+module Data.Derive.Example(Data2(..), guess) where
 
 import Language.Haskell.TH
 
@@ -6,9 +6,6 @@ import Language.Haskell.TH
 data Data2 a b = Ctor0
                | Ctor1 a
                | Ctor2 a b
-
-is :: a -> a
-is x = x
 
 
 guess :: Q [Dec] -> IO ()
