5c9221eb21ebf4945aaf16cb5a28c95452525b72
Neil Mitchell <ndmitchell@gmail.com>
2007-04-07 11:09:00 +0000

    Implement the start of the example guessing, can guess the instance context

--- Data/Derive/Example.hs
+++ Data/Derive/Example.hs
@@ -1,6 +1,8 @@
 module Data.Derive.Example(Data2(..), guess) where
 
-import Language.Haskell.TH
+import Language.Haskell.TH hiding (ppr)
+import Data.Derive.FixedPpr
+import Data.List
 
 
 data Data2 a b = Ctor0
@@ -9,5 +11,24 @@ data Data2 a b = Ctor0
 
 
 guess :: Q [Dec] -> IO ()
-guess x = error "Failed to guess (guessing not yet implemented)"
+guess x = runQ x >>= putStr . unlines . map guessDec
 
+
+guessDec :: Dec -> String
+guessDec (InstanceD ctx typ inner) =
+    "instance_context " ++ guessContext ctx ++ " " ++
+    guessPrinciple typ ++ " " ++ list (map guessDec inner)
+
+guessDec x = error $ show x
+
+
+
+guessContext = list . nub . map guessPrinciple
+
+guessPrinciple (AppT (ConT x) _) = show $ dropModule $ show x
+
+
+
+dropModule = reverse . takeWhile (/= '.') . reverse
+
+list x = "[" ++ concat (intersperse "," x) ++ "]"
