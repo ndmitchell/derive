b5ce283aa58a6cd49358d96d6b7b87dec27d07d6
Neil Mitchell <ndmitchell@gmail.com>
2007-04-08 11:21:53 +0000

    Add Data.Derive.All, which imports all derivers

---
+++ Data/Derive/All.hs
@@ -0,0 +1,20 @@
+
+module Data.Derive.All (
+    module Data.Derive.Binary,
+    module Data.Derive.BinaryDefer,
+    module Data.Derive.Data,
+    module Data.Derive.Eq,
+    module Data.Derive.Functor,
+    module Data.Derive.Ord,
+    module Data.Derive.Play,
+    module Data.Derive.Typeable
+    ) where
+
+import Data.Derive.Binary
+import Data.Derive.BinaryDefer
+import Data.Derive.Data
+import Data.Derive.Eq
+import Data.Derive.Functor
+import Data.Derive.Ord
+import Data.Derive.Play
+import Data.Derive.Typeable
