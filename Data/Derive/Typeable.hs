929f12b010b8c0a41eda27f8b4be3e207780f572
Neil Mitchell <ndmitchell@gmail.com>
2007-04-08 20:27:54 +0000

    Fix the deriving Typeable thing, so it works properly

--- Data/Derive/Typeable.hs
+++ Data/Derive/Typeable.hs
@@ -4,12 +4,12 @@ import Language.Haskell.TH.All
 import Data.Char
 
 makeTypeable = Derivation typeable' "Typeable"
-typeable' dat = [FunD nam [sclause [] (LitE $ StringL $ dataName dat)]
+typeable' dat = [FunD nam [sclause [] (l1 "mkTyCon" $ LitE $ StringL $ dataName dat)]
                 ,InstanceD [] hd [def]]
     where
         nam = mkName [if x == '.' then '_' else x | x <- "typename_" ++ dataName dat]
     
         n = if dataArity dat == 0 then "" else show (dataArity dat)
         hd = ConT (mkName $ "Typeable" ++ n) `AppT` ConT (mkName (dataName dat))
-        def = funN ("typeOf" ++ n) [sclause [WildP] (VarE nam)]
+        def = funN ("typeOf" ++ n) [sclause [WildP] (l2 "mkTyConApp" (VarE nam) nil)]
 
