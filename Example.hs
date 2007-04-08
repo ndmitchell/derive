fb781e17c811caf2cdceb689f2537a0ad312abdb
Neil Mitchell <ndmitchell@gmail.com>
2007-04-08 22:53:44 +0000

    Remove the examples directory, now the example is in the root and Derive.hs can run it

---
+++ Example.hs
@@ -0,0 +1,35 @@
+{-# OPTIONS_DERIVE --derive=Data,Typeable,Eq,Ord #-}
+
+module Example where
+
+
+data Foo = Bar
+
+
+data Color = RGB Int Int Int
+           | CMYK Int Int Int Int
+
+
+data Computer = Laptop { weight :: Int }
+              | Desktop { speed :: Int, memory :: Int }
+
+
+-- | All drinks mankind will ever need
+data Drinks = Beer Bool{-ale?-}
+            | Coffee
+            | Tea
+            | EnergyDrink
+            | Water
+            | Wine
+            | Whisky
+
+
+-- | A list with late failure
+data FailList e a = Nil | Fail e | Const a (FailList e a)
+                    deriving ( {-! Functor !-} )
+
+
+-- | State monad
+data State s a = StateT (s -> (s, a))
+                 deriving ( {-! Functor !-} )
+
