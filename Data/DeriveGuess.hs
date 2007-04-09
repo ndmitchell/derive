20cc8a7c33c9de099cbf376b3568338505d7b8c3
Neil Mitchell <ndmitchell@gmail.com>
2007-04-09 00:07:45 +0000

    Add basic hypothesis guessing - still need to figure out the App rule (everything else is easy)

--- Data/DeriveGuess.hs
+++ Data/DeriveGuess.hs
@@ -1,7 +1,6 @@
 module Data.DeriveGuess(Data2(..), guess) where
 
-import Language.Haskell.TH hiding (ppr)
-import Language.Haskell.TH.FixedPpr
+import Language.Haskell.TH.All
 import Data.List
 
 
@@ -19,8 +18,31 @@ guessDec (InstanceD ctx typ inner) =
     "instance_context " ++ guessContext ctx ++ " " ++
     guessPrinciple typ ++ " " ++ list (map guessDec inner)
 
+guessDec (FunD name claus) = "FunD " ++ show (show name) ++ " " ++ guessClauses claus
+
 guessDec x = error $ show x
 
+
+guessClauses :: [Clause] -> String
+guessClauses xs | null mid  = error "output raw"
+                | null h    = error "failed to find a hyp"
+                | otherwise = render (head h)
+    where
+        h = filter valid $ concatMap hyps mid
+    
+        (start,rest) = span isFree xs
+        (mid,stop) = break isFree rest
+
+        isFree _ = False
+        
+        
+        hyps (Clause pats (NormalB bod) []) = hypothesis (map f pats) bod
+            where
+                f WildP = Terms [] []
+                f (VarP x) = Terms [VarE x] []
+                f (ConP name xs) = Terms [ConE name] [VarE x | VarP x <- xs]
+
+        valid _ = error "guessClauses.valid"
 
 
 guessContext = list . nub . map guessPrinciple
@@ -32,3 +54,37 @@ guessPrinciple (AppT (ConT x) _) = show $ dropModule $ show x
 dropModule = reverse . takeWhile (/= '.') . reverse
 
 list x = "[" ++ concat (intersperse "," x) ++ "]"
+
+
+
+---------------------------------------------------------------------
+-- Hypothesis generation and testing
+
+
+data Terms = Terms {named :: [Exp], inductive :: [Exp]}
+             deriving Show
+
+
+type Hypothesis = Exp
+
+-- special meanings within Hypothesis:
+-- VarE #i_n, the n'th field of info i
+-- TupE [#fold[rn][10] x1 x2]
+--   r/n is reverse or normal
+--   1/0 is if there is a default or not
+--   x1 is the terminal element, if using 0
+--   x2 is the combiner (app2 it each time)
+
+
+hypothesis :: [Terms] -> Exp -> [Hypothesis]
+hypothesis info x = error $ "DeriveGuess.hypothsis: " ++ show (info,x)
+
+
+apply :: [Terms] -> Hypothesis -> Exp
+apply x y = error $ "DeriveGuess.apply: " ++ show (x,y)
+
+
+render :: Hypothesis -> String
+render x = error $ "DeriveGuess.render: " ++ show x
+
+
