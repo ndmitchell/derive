6e16380e978bc7a31d0f8f414fb837e030a09d0e
Neil Mitchell <ndmitchell@gmail.com>
2007-04-14 13:14:35 +0000

    Move to using derivation by guess for Eq

--- Data/Derive/Eq.hs
+++ Data/Derive/Eq.hs
@@ -2,27 +2,46 @@
 
 module Data.Derive.Eq(makeEq) where
 
-import Data.DeriveGuess
 import Language.Haskell.TH.All
 
+
+#ifdef GUESS
+
+import Data.DeriveGuess
+
+example = [d|
+
+    instance Eq a => Eq (DataName a) where
+        CtorZero == CtorZero = True
+        (CtorOne x1) == (CtorOne y1) = x1 == y1 && True
+        (CtorTwo x1 x2) == (CtorTwo y1 y2) = x1 == y1 && x2 == y2 && True
+        (CtorTwo' x1 x2) == (CtorTwo' y1 y2) = x1 == y1 && x2 == y2 && True
+        _ == _ = False
+
+    |]
+
+#endif
+
+
 makeEq = Derivation eq' "Eq"
+eq' dat = instance_context ["Eq"] "Eq" dat [FunD (mkName "==") ((map (\(ctorInd
+    ,ctor) -> (Clause [(ConP (mkName (ctorName ctor)) ((map (\field -> (VarP (
+    mkName ("x" ++ show field)))) (id [1..ctorArity ctor]))++[])),(ConP (mkName
+    (ctorName ctor)) ((map (\field -> (VarP (mkName ("y" ++ show field)))) (id
+    [1..ctorArity ctor]))++[]))] (NormalB (foldl1With (VarE (mkName "&&")) ((
+    map (\field -> (AppE (AppE (VarE (mkName "==")) (VarE (mkName ("x" ++ show
+    field)))) (VarE (mkName ("y" ++ show field))))) (id [1..ctorArity ctor]))++
+    [(ConE (mkName "True"))]++[]))) [])) (id (zip [0..] (dataCtors dat))))++[(
+    Clause [WildP,WildP] (NormalB (ConE (mkName "False"))) [])]++[])]
+
+
+{-
+-- HAND WRITTEN VERSION
+
 eq' dat = simple_instance "Eq" dat [funN "==" body]
     where
         body = map rule (dataCtors dat) ++ [defclause 2 false]
 
 rule ctor = sclause [ctp ctor 'a', ctp ctor 'b']
                     (and_ (zipWith (==:) (ctv ctor 'a') (ctv ctor 'b')))
-
-#if 0
-
-example = [d|
-
-    instance (Eq a, Eq b) => Eq (Data2 a b) where
-        Ctor0 == Ctor0 = True
-        (Ctor1 x) == (Ctor1 y) = x == y
-        (Ctor2 x1 x2) == (Ctor2 y1 y2) = x1 == y1 && x2 == y2
-        _ == _ = False
-
-    |]
-
-#endif
+-}
