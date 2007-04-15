5d50623e0090fa78dfa60cdeefda3cb99b1f6fae
Neil Mitchell <ndmitchell@gmail.com>
2007-04-15 17:21:28 +0000

    More changes to Play, now works for the Yhc.Core data type (finally!)

--- Data/Derive/Play.hs
+++ Data/Derive/Play.hs
@@ -41,7 +41,7 @@ getVar :: String -> Var String
 getVar x = do mp <- get
               let i = Map.findWithDefault 1 x mp
               put $ Map.insert x (i+1) mp
-              return $ x -- ++ if i == 1 then "" else show i
+              return $ x ++ if i == 1 then "" else show i
 
 runVar :: Var a -> a
 runVar x = evalState x Map.empty
@@ -49,7 +49,7 @@ runVar x = evalState x Map.empty
 
 
 play' dat =
-        [instance_default "Play" dat [funN "getChildren" gbody, funN "setChildren" rbody]]
+        [instance_default "Play" dat [funN "getChildren" gbody, funN "replaceChildren" rbody]]
     where
         ctors :: [(CtorDef,[Container])]
         ctors = [(c, map (typeToContainer (dataName dat)) (ctorTypes c)) | c <- dataCtors dat]
@@ -67,15 +67,20 @@ play' dat =
                     where vars = ['x':show i | i <- [1..length ts]]
 
 
-        rbody = [sclause [ctp (fst c) 'x'] (ritem c) | c <- ctors]
-
-        {-
-        [sclause [vr "x"] (case' (vr "x")
+        rbody = [Clause [vr "x"] (NormalB $ case' (vr "x")
                     [(ctp (fst c) 'x', tup [gitem c, ritem c]) | c <- ctors]
-                )]
-        -}
+                ) [funcList]]
         
         
+        funcList = funN "list'"
+                        [sclause [vr "f",nil] (runVar $ const1_ nil)
+                        ,sclause [vr "f",lK ":" [vr "x",vr "xs"]]
+                            (runVar $ do
+                                a <- const1_ (l0 ":")
+                                lhs <- join_ a (l1 "f" (vr "x"))
+                                join_ lhs (lK "list'" [vr "f",vr "xs"])
+                        )]
+
         
         wildcard :: Exp -> Exp
         wildcard x = everywhere (mkT f) x
@@ -90,24 +95,32 @@ play' dat =
         
         
         ritem :: (CtorDef,[Container]) -> Exp
-        ritem (c,ts) = wildcard $ runVar (value_ $ joins_ items)
+        ritem (c,ts) = wildcard $ runVar (value_ $ joins_ (l0 (ctorName c)) items)
             where
-                items = const1_ (l0 (ctorName c)) :
-                        [do q <- f t; return $ AppE q v | (t,v) <- zip ts (ctv c 'x')]
+                items = zip ts (ctv c 'x')
             
                 f None = const_
                 f Target = id_
-                f (List Target) = listId_
                 f (List t) = list_ (f t)
-                f (Tuple ts) = joins_ $ const1_ (l0 "tup") : map f ts
+                f (Tuple ts) = do
+                    vars1 <- replicateM (length ts) (getVar "x")
+                    vars2 <- replicateM (length ts) (getVar "y")
+                    bod <- joins_ (LamE (map vr vars1) (tup $ map vr vars1)) (zip ts (map vr vars2))
+                    return $ LamE [tup $ map vr vars2] bod
 
-        -- the replaceChildren combinators
+                joins_ root xs = do
+                    root2 <- const1_ root
+                    xs2 <- mapM (\(t,v) -> liftM (`appE` v) (f t)) xs
+                    foldM join_ root2 xs2
+
+
+       -- the replaceChildren combinators
         
         const_ = do
-            x <- getVar "x"
+            y <- getVar "y"
             ns <- getVar "ns"
             c <- getVar "c"
-            return $ lamE [vr x,vr ns,vr c] (app (vr c) [vr x,vr ns])
+            return $ lamE [vr y,vr ns,vr c] (app (vr c) [vr y,vr ns])
 
         id_ = do
             n <- getVar "n"
@@ -115,15 +128,17 @@ play' dat =
             c <- getVar "c"
             return $ lamE [WildP,lK ":" [vr n,vr ns],vr c] (app (vr c) [vr n,vr ns])
         
+        isId (LamE [WildP,ConP cons [VarP n, VarP ns],VarP c] (AppE (AppE (VarE c2) (VarE n2)) (VarE ns2)))
+            | show cons == ":" && n == n2 && c == c2 && ns == ns2 = True
+        isId _ = False
+        
         list_ x = liftM (l1 "list'") x
         
-        listId_ = return $ l0 "listId'"
-
         value_ inp = do
             inp <- inp
             ns <- getVar "ns"
-            x <- getVar "x"
-            return $ lamE [vr ns] $ appE2 inp (vr ns) $ lamE [vr x,WildP] (vr x)
+            y <- getVar "y"
+            return $ lamE [vr ns] $ appE2 inp (vr ns) $ lamE [vr y,WildP] (vr y)
 
         join_ ina inb = do
             ns1 <- getVar "ns"
@@ -139,20 +154,18 @@ play' dat =
 
         const1_ x = do c <- const_ ; return $ appE c x
 
-        joins_ x = do (y:ys) <- sequence x
-                      foldM join_ y ys
 
 
         -- can be a bit more fast and loose about variable clashes etc
         -- since we guarantee a limited set of generated values
 
         -- very special rule, only valid for play
-        appE (AppE (AppE (VarE l) x) y) (LamE (b:WildP:bs) c)
-            | show l == "listId'" = appE (lamE (b:bs) c) y
+        appE (AppE (AppE (AppE (VarE l) i) x) y) (LamE (b:WildP:bs) c)
+            | show l == "list'" && isId i = appE (lamE (b:bs) c) y
 
         appE (LamE (VarP x:xs) y) z = lamE xs (rebuild $ replaceVar x z y)
         appE (LamE (WildP :xs) y) z = lamE xs y
-        appE (AppE (LamE (x1@(ConP{}):VarP x2:xs) xb) y) z =
+        appE (AppE (LamE (x1:VarP x2:xs) xb) y) z =
               AppE (lamE (x1:xs) (rebuild $ replaceVar x2 z xb)) y
         appE x y = AppE x y
         
