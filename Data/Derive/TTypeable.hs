ab071bc9ef5d89d80106e4be0353c9301127453f
Stefan O'Rear <stefanor@cox.net>
2007-04-18 02:05:29 +0000

    Use new helpers in TTypeable

--- Data/Derive/TTypeable.hs
+++ Data/Derive/TTypeable.hs
@@ -9,21 +9,10 @@ import Data.Bits
 makeTTypeable = Derivation ttypeable' "TTypeable"
 ttypeable' dat = [InstanceD ctx hd []]
     where
-        l0t = ConT . mkName
-        l1t n x = AppT (l0t n) x
-        l2t n x y = AppT (l1t n x) y
-        lkt n l = foldl AppT (l0t n) l
-
-        hCons' = l2t "HCons"
-        hNil'  = l0t "HNil"
-
-        hSucc' = l1t "HSucc"
-        hZero' = l0t "HZero"
-
         bits x = [ if x .&. mask /= 0 then hSucc' hZero' else hZero' | mask <- [1,2,4,8,16,32,64,128] ]
         bitnam = foldr hCons' hNil' (concatMap (bits . ord) (dataName dat))
 
-        hd = l2t "TTypeable" (lkt (dataName dat) (vars 't')) (foldr hCons' hNil' (bitnam : vars 'n'))
-        ctx = zipWith (l2t "TTypeable") (vars 't') (vars 'n')
+        hd = l2 "TTypeable" (lK (dataName dat) (vrs 't')) (foldr hCons' hNil' (bitnam : vrs 'n'))
+        ctx = zipWith (l2 "TTypeable") (vrs 't') (vrs 'n')
 
-        vars ch = map (VarT . mkName . (ch:) . show) [ 1 .. dataArity dat ]
+        vrs = flip vars (dataArity dat)
