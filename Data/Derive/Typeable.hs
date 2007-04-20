00bca57c5a274a7528e8f5e6ca0694baa464b59d
Neil Mitchell <ndmitchell@gmail.com>
2007-04-20 13:24:32 +0000

    Change the Typeable derivation so it works on Hugs properly

--- Data/Derive/Typeable.hs
+++ Data/Derive/Typeable.hs
@@ -5,13 +5,45 @@ module Data.Derive.Typeable(makeTypeable) where
 import Language.Haskell.TH.All
 import Data.Char
 
+-- based on the macros in: http://darcs.haskell.org/packages/base/include/Typeable.h
+
+{-
+#define INSTANCE_TYPEABLE1(tycon,tcname,str) \
+tcname = mkTyCon str; \
+instance Typeable1 tycon where { typeOf1 _ = mkTyConApp tcname [] }; \
+instance Typeable a => Typeable (tycon a) where { typeOf = typeOfDefault }
+
+#define INSTANCE_TYPEABLE2(tycon,tcname,str) \
+tcname = mkTyCon str; \
+instance Typeable2 tycon where { typeOf2 _ = mkTyConApp tcname [] }; \
+instance Typeable a => Typeable1 (tycon a) where { \
+  typeOf1 = typeOf1Default }; \
+instance (Typeable a, Typeable b) => Typeable (tycon a b) where { \
+  typeOf = typeOfDefault }
+
+#define INSTANCE_TYPEABLE3(tycon,tcname,str) \
+tcname = mkTyCon str; \
+instance Typeable3 tycon where { typeOf3 _ = mkTyConApp tcname [] }; \
+instance Typeable a => Typeable2 (tycon a) where { \
+  typeOf2 = typeOf2Default }; \
+instance (Typeable a, Typeable b) => Typeable1 (tycon a b) where { \
+  typeOf1 = typeOf1Default }; \
+instance (Typeable a, Typeable b, Typeable c) => Typeable (tycon a b c) where { \
+  typeOf = typeOfDefault }
+-}
+
+
 makeTypeable = Derivation typeable' "Typeable"
-typeable' dat = [funN nam [sclause [] (l1 "mkTyCon" $ lit $ dataName dat)]
-                ,InstanceD [] hd [def]]
+typeable' dat = (funN nam [sclause [] (l1 "mkTyCon" $ lit $ dataName dat)])
+                : map f [0..dataArity dat]
     where
         nam = [if x == '.' then '_' else x | x <- "typename_" ++ dataName dat]
 
-        n = if dataArity dat == 0 then "" else show (dataArity dat)
-        hd = l1 ("Typeable" ++ n) (l0 (dataName dat))
-        def = funN ("typeOf" ++ n) [defclause 1 (l2 "mkTyConApp" (vr nam) nil)]
-
+        f n = InstanceD (map (l1 "Typeable") vars)
+                        (l1 ("Typeable"++sn) $ lK (dataName dat) vars)
+                        [funN ("typeOf"++sn) [sclause [WildP] def]]
+            where
+                def = if n == 0 then l2 "mkTyConApp" (l0 nam) (lst [])
+                                else l0 ("typeOf" ++ sn ++ "Default")
+                vars = take n $ map (vr . return) ['a'..]
+                sn = let i = dataArity dat - n in if i == 0 then "" else show i
