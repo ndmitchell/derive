a6f2afd132bcfd8471d67c0aea128e7e3612290e
Neil Mitchell <ndmitchell@gmail.com>
2007-05-10 13:43:39 +0000

    Add a Coarbitrary definition, thanks to Malcolm for the instance

--- Data/Derive/Arbitrary.hs
+++ Data/Derive/Arbitrary.hs
@@ -38,23 +38,32 @@ example = (,) "Arbitrary" [d|
                         x2 <- arbitrary
                         return (CtorTwo' x1 x2)
 
-        coarbitrary = error "coarbitrary not yet supported"
-
+        coarbitrary CtorZero = variant 0
+        coarbitrary (CtorOne x1) = variant 1 . coarbitrary x1
+        coarbitrary (CtorTwo x1 x2) = variant 2 . coarbitrary x1 . coarbitrary x2
+        coarbitrary (CtorTwo' x1 x2) = variant 3 . coarbitrary x1 . coarbitrary x2
     |]
 
 #endif
 
 makeArbitrary :: Derivation
 makeArbitrary = Derivation arbitrary' "Arbitrary"
-arbitrary' dat = [instance_context ["Arbitrary"] "Arbitrary" dat [(ValD (VarP (
-    mkName "arbitrary")) (NormalB (DoE [(BindS (VarP (mkName "x")) (AppE (VarE
-    (mkName "choose")) (TupE [(LitE (IntegerL 0)),(LitE (IntegerL (toInteger (
-    length (dataCtors dat) - 1))))]))),(NoBindS (CaseE (VarE (mkName "x")) ((
-    map (\(ctorInd,ctor) -> (Match (LitP (IntegerL ctorInd)) (NormalB (DoE ((
-    map (\field -> (BindS (VarP (mkName ("x" ++ show field))) (VarE (mkName
+arbitrary' dat = [InstanceD (concat ([(map (\tdat -> (AppT (ConT (mkName
+    "Arbitrary")) tdat)) (dataVars dat))])) (head [(AppT (ConT (mkName
+    "Arbitrary")) (lK (dataName dat) (dataVars dat)))])[(ValD (VarP (mkName
+    "arbitrary")) (NormalB (DoE [(BindS (VarP (mkName "x")) (AppE (VarE (mkName
+    "choose")) (TupE [(LitE (IntegerL 0)),(LitE (IntegerL (toInteger (length (
+    dataCtors dat) - 1))))]))),(NoBindS (CaseE (VarE (mkName "x")) ((map (\(
+    ctorInd,ctor) -> (Match (LitP (IntegerL ctorInd)) (NormalB (DoE ((map (
+    \field -> (BindS (VarP (mkName ("x" ++ show field))) (VarE (mkName
     "arbitrary")))) (id [1..ctorArity ctor]))++[(NoBindS (AppE (VarE (mkName
     "return")) (applyWith (ConE (mkName ("" ++ ctorName ctor))) ((map (\field
     -> (VarE (mkName ("x" ++ show field)))) (id [1..ctorArity ctor]))++[]))))]
-    ++[]))) [])) (id (zip [0..] (dataCtors dat))))++[])))])) []),(ValD (VarP (
-    mkName "coarbitrary")) (NormalB (AppE (VarE (mkName "error")) (LitE (
-    StringL "coarbitrary not yet supported")))) [])]]
+    ++[]))) [])) (id (zip [0..] (dataCtors dat))))++[])))])) []),(FunD (mkName
+    "coarbitrary") ((map (\(ctorInd,ctor) -> (Clause [(ConP (mkName ("" ++
+    ctorName ctor)) ((map (\field -> (VarP (mkName ("x" ++ show field)))) (id [
+    1..ctorArity ctor]))++[]))] (NormalB (foldl1With (VarE (mkName ".")) ([(
+    AppE (VarE (mkName "variant")) (LitE (IntegerL ctorInd)))]++(map (\field ->
+    (AppE (VarE (mkName "coarbitrary")) (VarE (mkName ("x" ++ show field))))) (
+    id [1..ctorArity ctor]))++[]))) [])) (id (zip [0..] (dataCtors dat))))++[])
+    )]]
