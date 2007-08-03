c3731f6998fe9ca25e4094032825f9c669f2d276
Samuel Bronson <naesten@gmail.com>
2007-08-03 18:59:23 +0000

    More tests. (Functor fails now.)

--- tests/Small.expected.hs
+++ tests/Small.expected.hs
@@ -1,6 +1,7 @@
 module SmallInstancess where
 import Small
 import Data.Generics
+import Data.Monoid
 
 instance Eq t1 => Eq (Expr t1)
     where (==) (ELambda x1 x2) (ELambda y1
@@ -21,21 +22,21 @@ instance Ord t1 => Ord (Expr t1)
                             tag (EVar {}) = 2
 
 instance Read t1 => Read (Expr t1)
-    where readsPrec p0 r0 = readParen (p0 > 10) [(ELambda x1 x2,
-                                                  r3) | ("ELambda", r1) <- lex r0,
-                                                        (x1, r2) <- readsPrec 11 r1,
-                                                        (x2,
-                                                         r3) <- readsPrec 11 r2] ++ (readParen (p0 > 10) [(EAp x1 x2,
-                                                                                                           r3) | ("EAp",
-                                                                                                                  r1) <- lex r0,
-                                                                                                                 (x1,
-                                                                                                                  r2) <- readsPrec 11 r1,
-                                                                                                                 (x2,
-                                                                                                                  r3) <- readsPrec 11 r2] ++ readParen (p0 > 10) [(EVar x1,
-                                                                                                                                                                   r2) | ("EVar",
-                                                                                                                                                                          r1) <- lex r0,
-                                                                                                                                                                         (x1,
-                                                                                                                                                                          r2) <- readsPrec 11 r1])
+    where readsPrec p0 r = readParen (p0 > 10) (\r0 -> [(ELambda x1 x2,
+                                                         r3) | ("ELambda", r1) <- lex r0,
+                                                               (x1, r2) <- readsPrec 11 r1,
+                                                               (x2,
+                                                                r3) <- readsPrec 11 r2]) r ++ (readParen (p0 > 10) (\r0 -> [(EAp x1 x2,
+                                                                                                                             r3) | ("EAp",
+                                                                                                                                    r1) <- lex r0,
+                                                                                                                                   (x1,
+                                                                                                                                    r2) <- readsPrec 11 r1,
+                                                                                                                                   (x2,
+                                                                                                                                    r3) <- readsPrec 11 r2]) r ++ readParen (p0 > 10) (\r0 -> [(EVar x1,
+                                                                                                                                                                                                r2) | ("EVar",
+                                                                                                                                                                                                       r1) <- lex r0,
+                                                                                                                                                                                                      (x1,
+                                                                                                                                                                                                       r2) <- readsPrec 11 r1]) r)
 
 instance Show t1 => Show (Expr t1)
     where showsPrec p (ELambda x1
@@ -54,3 +55,144 @@ instance (Data t1, Typeable t1) => Data (Expr t1)
     where gfoldl k r (ELambda x1 x2) = k (k (r ELambda) x1) x2
           gfoldl k r (EAp x1 x2) = k (k (r EAp) x1) x2
           gfoldl k r (EVar x1) = k (r EVar) x1
+
+instance Eq Primary
+    where (==) (Red) (Red) = True
+          (==) (Green) (Green) = True
+          (==) (Blue) (Blue) = True
+          (==) _ _ = False
+
+instance Ord Primary
+    where compare a b = check a b
+                      where check (Red) (Red) = compare () ()
+                            check (Green) (Green) = compare () ()
+                            check (Blue) (Blue) = compare () ()
+                            check a b = compare (tag a) (tag b)
+                            tag (Red {}) = 0
+                            tag (Green {}) = 1
+                            tag (Blue {}) = 2
+
+instance Enum Primary
+    where toEnum 0 = Red{}
+          toEnum 1 = Green{}
+          toEnum 2 = Blue{}
+          toEnum n = error ((++) "toEnum " ((++) (show n) ", not defined for Primary"))
+          fromEnum (Red {}) = 0
+          fromEnum (Green {}) = 1
+          fromEnum (Blue {}) = 2
+
+instance Bounded Primary
+    where minBound = Red
+          maxBound = Blue
+
+instance Read Primary
+    where readsPrec p0 r = readParen (p0 > 10) (\r0 -> [(Red,
+                                                         r1) | ("Red",
+                                                                r1) <- lex r0]) r ++ (readParen (p0 > 10) (\r0 -> [(Green,
+                                                                                                                    r1) | ("Green",
+                                                                                                                           r1) <- lex r0]) r ++ readParen (p0 > 10) (\r0 -> [(Blue,
+                                                                                                                                                                              r1) | ("Blue",
+                                                                                                                                                                                     r1) <- lex r0]) r)
+
+instance Show Primary
+    where showsPrec p (Red) = showParen (p > 10) (showString "Red")
+          showsPrec p (Green) = showParen (p > 10) (showString "Green")
+          showsPrec p (Blue) = showParen (p > 10) (showString "Blue")
+
+typename_Primary = mkTyCon "Primary"
+instance Typeable Primary
+    where typeOf _ = mkTyConApp typename_Primary []
+
+instance Data Primary
+    where gfoldl k r (Red) = r Red
+          gfoldl k r (Green) = r Green
+          gfoldl k r (Blue) = r Blue
+
+instance Eq t1 => Eq (BinTree t1)
+    where (==) (Leaf) (Leaf) = True
+          (==) (Branch x1 x2 x3) (Branch y1
+                                         y2
+                                         y3) = (&&) ((==) x1 y1) ((&&) ((==) x2 y2) ((==) x3 y3))
+          (==) _ _ = False
+
+instance Ord t1 => Ord (BinTree t1)
+    where compare a b = check a b
+                      where check (Leaf) (Leaf) = compare () ()
+                            check (Branch x1 x2 x3) (Branch y1 y2 y3) = compare (x1,
+                                                                                 x2,
+                                                                                 x3) (y1, y2, y3)
+                            check a b = compare (tag a) (tag b)
+                            tag (Leaf {}) = 0
+                            tag (Branch {}) = 1
+
+instance Read t1 => Read (BinTree t1)
+    where readsPrec p0 r = readParen (p0 > 10) (\r0 -> [(Leaf,
+                                                         r1) | ("Leaf",
+                                                                r1) <- lex r0]) r ++ readParen (p0 > 10) (\r0 -> [(Branch x1 x2 x3,
+                                                                                                                   r4) | ("Branch",
+                                                                                                                          r1) <- lex r0,
+                                                                                                                         (x1,
+                                                                                                                          r2) <- readsPrec 11 r1,
+                                                                                                                         (x2,
+                                                                                                                          r3) <- readsPrec 11 r2,
+                                                                                                                         (x3,
+                                                                                                                          r4) <- readsPrec 11 r3]) r
+
+instance Show t1 => Show (BinTree t1)
+    where showsPrec p (Leaf) = showParen (p > 10) (showString "Leaf")
+          showsPrec p (Branch x1
+                              x2
+                              x3) = showParen (p > 10) (showString "Branch" . (showChar ' ' . (showsPrec 11 x1 . (showChar ' ' . (showsPrec 11 x2 . (showChar ' ' . showsPrec 11 x3))))))
+
+typename_BinTree = mkTyCon "BinTree"
+instance Typeable1 BinTree
+    where typeOf1 _ = mkTyConApp typename_BinTree []
+instance Typeable a => Typeable (BinTree a)
+    where typeOf = typeOfDefault
+
+instance (Data t1, Typeable t1) => Data (BinTree t1)
+    where gfoldl k r (Leaf) = r Leaf
+          gfoldl k r (Branch x1 x2 x3) = k (k (k (r Branch) x1) x2) x3
+
+instance Eq t1 => Eq (Id t1)
+    where (==) (Id x1) (Id y1) = (==) x1 y1
+          (==) _ _ = False
+
+instance Ord t1 => Ord (Id t1)
+    where compare a b = check a b
+                      where check (Id x1) (Id y1) = compare x1 y1
+                            check a b = compare (tag a) (tag b)
+                            tag (Id {}) = 0
+
+instance Enum (Id t1)
+    where toEnum 0 = Id{}
+          toEnum n = error ((++) "toEnum " ((++) (show n) ", not defined for Id"))
+          fromEnum (Id {}) = 0
+
+instance Bounded t1 => Bounded (Id t1)
+    where minBound = Id minBound
+          maxBound = Id maxBound
+
+instance Read t1 => Read (Id t1)
+    where readsPrec p0 r = readParen (p0 > 10) (\r0 -> [(Id x1,
+                                                         r2) | ("Id", r1) <- lex r0,
+                                                               (x1, r2) <- readsPrec 11 r1]) r
+
+instance Show t1 => Show (Id t1)
+    where showsPrec p (Id x1) = showParen (p > 10) (showString "Id" . (showChar ' ' . showsPrec 11 x1))
+
+typename_Id = mkTyCon "Id"
+instance Typeable1 Id
+    where typeOf1 _ = mkTyConApp typename_Id []
+instance Typeable a => Typeable (Id a)
+    where typeOf = typeOfDefault
+
+instance (Data t1, Typeable t1) => Data (Id t1)
+    where gfoldl k r (Id x1) = k (r Id) x1
+
+instance Functor (Id t1)
+    where fmap fun (Id a) = Id a
+
+instance Monoid t1 => Monoid (Id t1)
+    where mempty = Id mempty
+          mappend (Id x1) (Id y1) = Id (mappend x1 y1)
--- tests/Small.hs
+++ tests/Small.hs
@@ -2,8 +2,26 @@ module Small where
 
 import Data.Generics
 
+
 data Expr name = ELambda name (Expr name)
                | EAp (Expr name) (Expr name)
                | EVar name
                  deriving ({-! Eq, Ord, Read, Show, Typeable, Data !-})
 
+
+data Primary = Red | Green | Blue
+               deriving ({-! Eq, Ord, Enum, Bounded, Read, Show,
+                             Typeable, Data !-})
+
+
+data BinTree a = Leaf | Branch a (BinTree a) (BinTree a)
+                 deriving ({-! Eq, Ord, Read, Show, Typeable, Data !-})
+
+
+-- This doesn't work :-(
+
+-- newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
+--                      deriving ({-! Eq, Ord, Read, Show, Typeable, Data, Monoid !-})
+
+
+newtype Id a = Id a deriving ({-! Eq, Ord, Enum, Bounded, Read, Show, Typeable, Data, Functor, Monoid !-})
