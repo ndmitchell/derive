3571cc0df7f671f924f97951c06f7a2d1ae75488
Neil Mitchell <ndmitchell@gmail.com>
2007-08-04 17:54:21 +0000

    Add a basic test script to the Setup.hs file

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,33 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo
+
+import System.Directory
+import System.Cmd
+import System.Exit
+import Control.Monad
+
+
+main = defaultMainWithHooks defaultUserHooks{runTests=test}
+
+
+test :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
+test args bool pd lbi = do
+    putStrLn "Make sure you have installed the derive you wish to test!"
+    cur <- getCurrentDirectory
+    setCurrentDirectory "tests"
+    
+    b <- doesFileExist "Small.out"
+    when b $ removeFile "Small.out"
+    i <- system $ "derive -mSmallInstancess -iSmall -iData.Generics -iData.Monoid " ++
+                  "-o Small.out.hs Small.hs"
+
+    b <- doesFileExist "Small.out"
+    if b then system "diff -u Small.expected.hs Small.out.hs" >> return ()
+         else putStrLn "Error: Failed to generate the output file"
+    
+    -- currently we turn off warnings when compiling
+    -- it would be nice if we made the generated code compile without warnings
+    system "ghc --make -c -w Small.out.hs"
+    setCurrentDirectory cur
+
