e670f926b631689ca5447a3c262b4c5939d82cf2
Neil Mitchell <ndmitchell@gmail.com>
2008-05-04 09:14:46 +0000

    Add a pile more definitions

--- Test/Data.hs
+++ Test/Data.hs
@@ -6,28 +6,60 @@ module Test.Data(
 
 import Control.Monad
 import Data.DeriveTH
+import Data.List
 import Language.Haskell.TH.Syntax
 
 
+concatMapM f = liftM concat . mapM f
+
+
 derivess :: [Derivation] -> Q [Dec]
-derivess = liftM concat . mapM derives
+derivess = concatMapM derives
+
+
+derivesNot :: Derivation -> [Name] -> Q [Dec]
+derivesNot f bad = concatMapM (derive f) (names \\ bad)
 
 
 derives :: Derivation -> Q [Dec]
-derives f = liftM concat $ mapM (derive f) names
+derives f = concatMapM (derive f) names
 
 
 names :: [Name]
-names = [''A, ''B, ''Color]
-
-
-
-
-data Color = RGB Int Int Int
-           | CMYK Int Int Int Int
-           deriving (Show)
+names = [''A, ''B, ''Color, ''Computer, ''Drinks, ''FailList, ''State]
 
 
 data A a = A a [C a]
 data B a = B a
 type C a = B (B a)
+
+
+data Foo = Bar
+
+
+data Color = RGB Int Int Int
+           | CMYK Int Int Int Int
+
+
+data Computer = Laptop { weight :: Int }
+              | Desktop { speed :: Int, memory :: Int }
+
+
+-- | All drinks mankind will ever need
+data Drinks = Beer Bool{-ale?-}
+            | Coffee
+            | Tea
+            | EnergyDrink
+            | Water
+            | Wine
+            | Whisky
+
+
+-- | A list with late failure
+data FailList e a = Nil | Fail e | Const a (FailList e a)
+
+
+-- | State monad
+data State s a = StateT (s -> (s, a))
+
+
