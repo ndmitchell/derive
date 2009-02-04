944950908b88839d8b4ede4bf7b4813e07a7f7af
Samuel Bronson <naesten@gmail.com>
2009-02-04 20:44:19 +0000

    Test for Update and Is

---
+++ Test/UpdateIs.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Test.UpdateIs where
+
+import Test.Data
+import Data.DeriveTH
+
+data PolyRecord i = PR { pr1 :: i,
+                         pr2 :: [i] }
+
+$( derive makeUpdate ''Computer )
+$( derive makeUpdate ''PolyRecord )
+$( derive makeIs ''Computer )
+$( derive makeIs ''PolyRecord )
+
