7f58e3b95664eeea5f984d80f4cb193f9c190778
Neil Mitchell <ndmitchell@gmail.com>
2009-05-02 08:01:23 +0000

    Add support for the Fold bits

--- derive2/Apply.hs
+++ derive2/Apply.hs
@@ -7,13 +7,13 @@ import Data.Maybe
 
 
 apply :: Dat -> DSL -> Res
-apply dat dsl = fromUniverse $ apply2 dat Nothing Nothing dsl
+apply dat dsl = fromUniverse $ apply2 dat Nothing Nothing Nothing dsl
 
 
-apply2 :: Dat -> Maybe Ctr -> Maybe Int -> DSL -> Universe
-apply2 dat ctr fld = f
+apply2 :: Dat -> Maybe Ctr -> Maybe Int -> Maybe (Universe,Universe) -> DSL -> Universe
+apply2 dat ctr fld lst = f
     where
-        app2 = apply2 dat ctr fld
+        app2 = apply2 dat ctr fld lst
     
         f (Instance ctx hd body) =
                 UApp "InstDecl" [uni con, uni $ UnQual $ Ident hd, uni [typ], app2 body]
@@ -21,18 +21,24 @@ apply2 dat ctr fld = f
                 con = [ClassA (UnQual $ Ident c) [TyVar $ Ident v] | v <- dataVars dat, c <- ctx]
                 typ = foldl TyApp (TyCon $ UnQual $ Ident $ dataName dat) [TyVar $ Ident v | v <- dataVars dat]
 
-        f (MapCtor dsl) = UList [apply2 dat (Just c) Nothing dsl | c <- dataCtors dat]
-        f (MapField dsl) = UList [apply2 dat ctr (Just i) dsl | i <- [1..ctorFields $ fromJust ctr]]
+        f (MapCtor dsl) = UList [apply2 dat (Just c) Nothing lst dsl | c <- dataCtors dat]
+        f (MapField dsl) = UList [apply2 dat ctr (Just i) lst dsl | i <- [1..ctorFields $ fromJust ctr]]
 
         f CtorName = UString $ ctorName $ fromJust ctr
         f FieldInd = UInt $ fromJust fld
+
+        f Head = fst $ fromJust lst
+        f Tail = snd $ fromJust lst
+        f (Fold cons nil xs) = case app2 xs of UList xs -> g xs
+            where
+                g [] = app2 nil
+                g (x:xs) = apply2 dat ctr fld (Just (x,g xs)) cons
 
         f (List xs) = UList $ map f xs
         f (Append x y) = case (app2 x, app2 y) of
             (UList x, UList y) -> UList (x++y)
             (UString x, UString y) -> UString (x++y)
         f (String x) = UString x
-        f (ShowInt x) = case app2 x of
-            UInt x -> UString (show x)
+        f (ShowInt x) = case app2 x of UInt x -> UString (show x)
         f (App x ys) = UApp x $ map app2 ys
         f dsl = error $ "app: " ++ show dsl
--- derive2/DSL.hs
+++ derive2/DSL.hs
@@ -14,6 +14,10 @@ data DSL = App String [DSL]
          | CtorName
          | FieldInd
          
+         | Fold DSL DSL DSL
+         | Head
+         | Tail
+         
          | Instance [String] String DSL{-[InstDecl]-}
            deriving Show
 
@@ -29,6 +33,7 @@ fromUni x = error $ show ("fromUni",x)
 
 _1 s x1 = App s [x1]
 _2 s x1 x2 = App s [x1,x2]
+_3 s x1 x2 x3 = App s [x1,x2,x3]
 _5 s x1 x2 x3 x4 x5 = App s [x1,x2,x3,x4,x5]
 
 u x = fromUni $ uni x
@@ -36,9 +41,12 @@ u x = fromUni $ uni x
 dslEq :: DSL
 dslEq = singleton $ Instance ["Eq"] "Eq" $ singleton $ _1 "InsDecl" $ _1 "FunBind" $ match `Append` dull
     where
-        match = MapCtor $ _5 "Match" (u $ Symbol "==") (List [vars "x",vars "y"]) (u (Nothing :: Maybe Type)) (u $ UnGuardedRhs $ Con $ UnQual $ Ident "False") (u $ BDecls [])
+        match = MapCtor $ _5 "Match" (u $ Symbol "==") (List [vars "x",vars "y"]) (u (Nothing :: Maybe Type)) (_1 "UnGuardedRhs" bod) (u $ BDecls [])
         vars x = _2 "PApp" (_1 "UnQual" $ _1 "Ident" CtorName) (MapField (_1 "PVar" $ _1 "Ident" $ Append (String x) (ShowInt FieldInd)))
-        
+        bod = Fold (_3 "InfixApp" Head (u $ QVarOp $ UnQual $ Symbol "&&") Tail) (u $ Con $ UnQual $ Ident "True") $ MapField pair
+        pair = _3 "InfixApp" (var "x") (u $ QVarOp $ UnQual $ Symbol "==") (var "y")
+        var x = _1 "Var" $ _1 "UnQual" $ _1 "Ident" $ Append (String x) (ShowInt FieldInd)
+
         dull = u [Match sl (Symbol "==") [PWildCard,PWildCard] Nothing (UnGuardedRhs $ Con $ UnQual $ Ident "False") (BDecls [])]
 
 {-
