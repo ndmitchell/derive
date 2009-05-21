387b78b3f580700d15e6747e1132a9009ab3e515
Neil Mitchell <ndmitchell@gmail.com>
2009-05-21 18:25:06 +0000

    Use the Apply function as refactored for the paper

--- derive2/Apply.hs
+++ derive2/Apply.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 
 module Apply(apply, applyEnv, Env(..)) where
 
@@ -11,55 +12,53 @@ apply :: DSL -> Input -> Out
 apply dsl input = fromOutput $ applyEnv dsl Env{envInput=input}
 
 
-data Env = Env
-    {envInput :: Input
-    ,envCtor :: Ctor
-    ,envField :: Integer
-    ,envFold :: (Output,Output)
-    }
+data Env = Env  {envInput :: Input
+                ,envCtor :: Ctor
+                ,envField :: Integer
+                ,envFold :: (Output,Output) }
 
 applyEnv :: DSL -> Env -> Output
 applyEnv dsl env@(Env input ctor field fold) = f dsl
     where
-        dataVariables = genericTake (dataVars input) $ map (:[]) ['a'..]
-    
-        f (Instance ctx hd body) =
-                OApp "InstDecl" [out con, out $ UnQual $ Ident hd, out [typ], f body]
-            where
-                con = [ClassA (UnQual $ Ident c) [TyVar $ Ident v] | v <- dataVariables, c <- ctx]
-                typ = foldl TyApp (TyCon $ UnQual $ Ident $ dataName input) [TyVar $ Ident v | v <- dataVariables]
-        
-        f (Application xs) = case f xs of
-            OList xs -> g xs
-            where g [x] = x
-                  g xs = OApp "App" [g $ init xs, last xs]
+    vars = genericTake (dataVars input) $ map (:[]) ['a'..]
 
-        f (MapCtor dsl) = OList [applyEnv dsl env{envCtor=c} | c <- dataCtors input]
-        f (MapField dsl) = OList [applyEnv dsl env{envField=i} | i <- [1.. fromIntegral $ ctorArity ctor]]
+    f (Instance ctx hd body) =
+        OApp "InstDecl"
+            [out
+                [ClassA (UnQual $ Ident c) [TyVar $ Ident v]
+                | v <- vars, c <- ctx]
+            , out $ UnQual $ Ident hd
+            , out [foldl TyApp
+                (TyCon $ UnQual $ Ident $ dataName input)
+                [TyVar $ Ident v | v <- vars]]
+            , f body]
 
-        f DataName = OString $ dataName input
-        f CtorName = OString $ ctorName ctor
-        f CtorArity = OInt $ ctorArity ctor
-        f CtorIndex = OInt $ ctorIndex ctor
-        f FieldIndex = OInt $ field
+    f (Application (f -> OList xs)) =
+        foldl1 (\a b -> OApp "App" [a,b]) xs
 
-        f Head = fst fold
-        f Tail = snd fold
-        f (Fold cons xs) = case f xs of OList xs -> g xs
-            where g [x] = x
-                  g (x:xs) = applyEnv cons env{envFold=(x,g xs)}
+    f (MapCtor dsl) = OList  [  applyEnv dsl env{envCtor=c}
+         |  c <- dataCtors input]
+    f (MapField dsl) = OList [applyEnv dsl env{envField=i}
+         | i <- [1.. fromIntegral $ ctorArity ctor]]
 
-        f (List xs) = OList $ map f xs
-        f (Reverse xs) = case f xs of
-            OList xs -> OList $ reverse xs
-        f (Concat xs) = case f xs of
-            OList [] -> OList []
-            OList xs -> foldr1 g xs
-        f (String x) = OString x
-        f (Int x) = OInt x
-        f (ShowInt x) = case f x of OInt x -> OString (show x)
-        f (App x ys) = case f ys of
-            OList ys -> OApp x ys
+    f DataName = OString $ dataName input
+    f CtorName = OString $ ctorName ctor
+    f CtorArity = OInt $ ctorArity ctor
+    f CtorIndex = OInt $ ctorIndex ctor
+    f FieldIndex = OInt $ field
 
-        g (OList x) (OList y) = OList (x++y)
-        g (OString x) (OString y) = OString (x++y)
+    f Head = fst fold
+    f Tail = snd fold
+    f (Fold cons (f -> OList xs)) =
+        foldr1 (\a b -> applyEnv cons env{envFold=(a,b)}) xs
+
+    f (List xs) = OList $ map f xs
+    f (Reverse (f -> OList xs)) = OList $ reverse xs
+    f (Concat (f -> OList [])) = OList []
+    f (Concat (f -> OList xs)) = foldr1 g xs
+            where  g (OList    x) (OList    y) = OList    (x++y)
+                   g (OString  x) (OString  y) = OString  (x++y)
+    f (String x) = OString x
+    f (Int x) = OInt x
+    f (ShowInt (f -> OInt x)) = OString $ show x
+    f (App x (f -> OList ys)) = OApp x ys
