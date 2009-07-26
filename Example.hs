8a51a1da7f5382b4b284d8760b182328c8d566e7
Neil Mitchell <ndmitchell@gmail.com>
2009-07-26 07:35:40 +0000

    Simplify the example, now we have better tests

--- Example.hs
+++ Example.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_DERIVE --derive=Data,Eq ,Typeable,Eq,Ord #-}
+{-# OPTIONS_DERIVE --derive=Eq,Ord,Eq #-}
 
 module Example where
 
@@ -13,23 +13,7 @@ data Color = RGB Int Int Int
 data Computer = Laptop { weight :: Int }
               | Desktop { speed :: Int, memory :: Int }
 
-
--- | All drinks mankind will ever need
-data Drinks = Beer Bool{-ale?-}
-            | Coffee
-            | Tea
-            | EnergyDrink
-            | Water
-            | Wine
-            | Whisky
-
-
--- | A list with late failure
-data FailList e a = Nil | Fail e | Const a (FailList e a)
-                    deriving ( {- ! Functor !-} )
-
-
 -- | State monad
 data State s a = StateT (s -> (s, a))
-                 deriving ( {- ! Functor !-} )
+                 deriving ( {-! Functor !-} )
 
