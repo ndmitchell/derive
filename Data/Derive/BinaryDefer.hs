8ee1e16b6eb918c4c551051119d188a3b5fbce85
Neil Mitchell <ndmitchell@gmail.com>
2009-08-06 10:44:24 +0000

    Fix the definition of BinaryDefer, was wrong before

--- Data/Derive/BinaryDefer.hs
+++ Data/Derive/BinaryDefer.hs
@@ -5,9 +5,17 @@ import "binarydefer" Data.Binary.Defer
 example :: Sample
 
 instance BinaryDefer a => BinaryDefer (Sample a) where
-    bothDefer = defer [\ ~(First) -> unit First
-                      ,\ ~(Second x1 x2) -> unit Second << x1 << x2
-                      ,\ ~(Third x1) -> unit Third << x1
+    bothDefer = defer [\ ~(o@(First)) -> if null [] then unit (First) <<! o else unit First
+                      ,\ ~(o@(Second x1 x2)) -> if null [const () x1, const () x2] then unit (Second x1 x2) <<! o else unit Second << x1 << x2
+                      ,\ ~(o@(Third x1)) -> if null [const () x1] then unit (Third x1) <<! o else unit Third << x1
+                      ]
+
+test :: FailList
+
+instance (BinaryDefer e, BinaryDefer a) => BinaryDefer (FailList e a) where
+    bothDefer = defer [\ ~(o@Zoro) -> unit Zoro <<! o
+                      ,\ ~(o@(Fail x1)) -> unit Fail << x1
+                      ,\ ~(o@(Const x1 x2)) -> unit Const << x1 << x2
                       ]
 
 -}
@@ -26,15 +34,30 @@ dslBinaryDefer =
     (List [App "App" (List [App "Var" (List [App "UnQual" (List [App
     "Ident" (List [String "defer"])])]),App "List" (List [MapCtor (App
     "Lambda" (List [List [App "PIrrPat" (List [App "PParen" (List [App
-    "PApp" (List [App "UnQual" (List [App "Ident" (List [CtorName])]),
-    MapField (App "PVar" (List [App "Ident" (List [Concat (List [
-    String "x",ShowInt FieldIndex])])]))])])])],Fold (App "InfixApp" (
-    List [Tail,App "QVarOp" (List [App "UnQual" (List [App "Symbol" (
-    List [String "<<"])])]),Head])) (Concat (List [Reverse (MapField (
+    "PAsPat" (List [App "Ident" (List [String "o"]),App "PParen" (List
+    [App "PApp" (List [App "UnQual" (List [App "Ident" (List [CtorName
+    ])]),MapField (App "PVar" (List [App "Ident" (List [Concat (List [
+    String "x",ShowInt FieldIndex])])]))])])])])])],App "If" (List [
+    App "App" (List [App "Var" (List [App "UnQual" (List [App "Ident"
+    (List [String "null"])])]),App "List" (List [MapField (Application
+    (List [App "Var" (List [App "UnQual" (List [App "Ident" (List [
+    String "const"])])]),App "Con" (List [App "Special" (List [App
+    "UnitCon" (List [])])]),App "Var" (List [App "UnQual" (List [App
+    "Ident" (List [Concat (List [String "x",ShowInt FieldIndex])])])])
+    ]))])]),App "InfixApp" (List [App "App" (List [App "Var" (List [
+    App "UnQual" (List [App "Ident" (List [String "unit"])])]),App
+    "Paren" (List [Application (Concat (List [List [App "Con" (List [
+    App "UnQual" (List [App "Ident" (List [CtorName])])])],MapField (
     App "Var" (List [App "UnQual" (List [App "Ident" (List [Concat (
-    List [String "x",ShowInt FieldIndex])])])]))),List [App "App" (
-    List [App "Var" (List [App "UnQual" (List [App "Ident" (List [
-    String "unit"])])]),App "Con" (List [App "UnQual" (List [App
-    "Ident" (List [CtorName])])])])]]))]))])])]),App "BDecls" (List [
-    List []])])])])]
+    List [String "x",ShowInt FieldIndex])])])]))]))])]),App "QVarOp" (
+    List [App "UnQual" (List [App "Symbol" (List [String "<<!"])])]),
+    App "Var" (List [App "UnQual" (List [App "Ident" (List [String "o"
+    ])])])]),Fold (App "InfixApp" (List [Tail,App "QVarOp" (List [App
+    "UnQual" (List [App "Symbol" (List [String "<<"])])]),Head])) (
+    Concat (List [Reverse (MapField (App "Var" (List [App "UnQual" (
+    List [App "Ident" (List [Concat (List [String "x",ShowInt
+    FieldIndex])])])]))),List [App "App" (List [App "Var" (List [App
+    "UnQual" (List [App "Ident" (List [String "unit"])])]),App "Con" (
+    List [App "UnQual" (List [App "Ident" (List [CtorName])])])])]]))]
+    )]))])])]),App "BDecls" (List [List []])])])])]
 -- GENERATED STOP
