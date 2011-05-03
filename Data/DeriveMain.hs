47828962730aa6d56393977dd515cb9f494c1a13
Neil Mitchell <ndmitchell@gmail.com>
2011-05-03 18:35:27 +0000

    Improve the documentation for deriveMain, contributed by Benedikt Huber

--- Data/DeriveMain.hs
+++ Data/DeriveMain.hs
@@ -1,5 +1,19 @@
+{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- Needed for Haddock docs
 
--- | Use this module to create your own derive program, with additional derivations.
+-- | Use this module to create your own derive program, supporting custom derivations.
+--   As an example:
+--
+-- @
+--   import Data.DeriveMain
+--   import Data.Derive.All        (derivations)
+--   import MyProject.MyDerivation (myDerivation)
+-- @
+--
+-- @
+--   main :: IO
+--   main = 'deriveMain' $ [myDerivation] ++ 'derivations'
+-- @
 module Data.DeriveMain(deriveMain) where
 
 import Derive.Main
+import Data.Derive.All(derivations)
