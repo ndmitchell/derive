4a8837bc2b47b0a6e40e4aa87c1374b7162d53f9
Neil Mitchell <ndmitchell@gmail.com>
2012-01-01 16:46:34 +0000

    Modify Example.hs so it works with the GHC preprocessor directly

--- Example.hs
+++ Example.hs
@@ -1,23 +1,20 @@
-{-# OPTIONS_DERIVE --derive=Eq,Ord,Eq #-}
+{-# OPTIONS_GHC -F -pgmFderive -optF-F #-}
+{-# OPTIONS_DERIVE --derive=Eq,Ord #-}
 
 module Example where
 
-
 data Foo = Bar
-
 
 data Color = RGB Int Int Int
            | CMYK Int Int Int Int
 
-
 data Computer = Laptop { weight :: Int }
               | Desktop { speed :: Int, memory :: Int }
-
--- | State monad
-data State s a = StateT (s -> (s, a))
-                 deriving ( {-! Functor !-} )
-
+                deriving ( {-! Show !-} )
 
 {-!
 deriving instance Show Color
 !-}
+
+main = print (RGB 1 2 3 == CMYK 1 2 3 4, RGB 1 2 3)
+
