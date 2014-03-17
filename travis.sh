de387f077cc51d76f289ae2268c3fe42e7243cbe
Neil Mitchell <ndmitchell@gmail.com>
2014-03-17 11:12:33 +0000

    Add a test suite

---
+++ travis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e -x
+wget https://raw.github.com/ndmitchell/neil/master/travis.sh -O - --no-check-certificate --no-cache --quiet | sh
+cabal install cereal json binarydefer data-lens
+runhaskell Main --generate
+runhaskell Main --test
