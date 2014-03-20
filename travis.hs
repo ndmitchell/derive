cb125c8fbb71fed623f86848e4e1271aaa621edb
Neil Mitchell <ndmitchell@gmail.com>
2014-03-20 14:32:02 +0000

    Remove the --verbose from cabal install

--- travis.hs
+++ travis.hs
@@ -2,6 +2,6 @@
 import Neil
 
 main = do
-    retry 3 $ cmd "cabal install cereal json binarydefer data-lens QuickCheck --verbose"
+    retry 3 $ cmd "cabal install cereal json binarydefer data-lens QuickCheck"
     cmd "runhaskell Main --generate"
     cmd "runhaskell Main --test"
