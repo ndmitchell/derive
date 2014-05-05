4d80494d02044f9de7729c9e50990d0fa1bf44dd
Neil Mitchell <ndmitchell@gmail.com>
2014-05-05 21:35:39 +0100

    Add --force-reinstalls to the reinstall command line

--- travis.hs
+++ travis.hs
@@ -2,6 +2,6 @@
 import Neil
 
 main = do
-    retry 3 $ cmd "cabal install cereal json binarydefer data-lens QuickCheck"
+    retry 3 $ cmd "cabal install cereal json binarydefer data-lens QuickCheck --force-reinstalls"
     cmd "runhaskell Main --generate"
     cmd "runhaskell Main --test"
