765b4cf13e1f0d1e8c7ae44d1cea0334491b1a84
Neil Mitchell <ndmitchell@gmail.com>
2014-10-12 21:49:32 +0100

    Print out if generation changes anything

--- travis.hs
+++ travis.hs
@@ -4,4 +4,8 @@ import Neil
 main = do
     retry 3 $ cmd "cabal install cereal json binarydefer data-lens QuickCheck --force-reinstalls"
     cmd "runhaskell Main --generate"
+    cmd "git checkout > temp.txt" -- should have no stdout
+    src <- readFile "temp.txt"
+    print src
+    when (lines src /= []) $ error "generating changed something!"
     cmd "runhaskell Main --test"
