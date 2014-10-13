10bd849471ae808784d8ba1ae42178a3d8351b30
Neil Mitchell <ndmitchell@gmail.com>
2014-10-13 07:37:53 +0100

    Change the generator check to use git

--- travis.hs
+++ travis.hs
@@ -4,8 +4,5 @@ import Neil
 main = do
     retry 3 $ cmd "cabal install cereal json binarydefer data-lens QuickCheck --force-reinstalls"
     cmd "runhaskell Main --generate"
-    cmd "git checkout > temp.txt" -- should have no stdout
-    src <- readFile "temp.txt"
-    print src
-    when (lines src /= []) $ error "generating changed something!"
+    cmd "git diff --exit-code"
     cmd "runhaskell Main --test"
