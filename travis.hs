97b51529211c2f9151060c0b5832e13098946e46
Neil Mitchell <ndmitchell@gmail.com>
2015-03-16 09:55:40 +0000

    If installation of dependencies fails on GHC head then don't worry

--- travis.hs
+++ travis.hs
@@ -1,9 +1,13 @@
 
 import System.Process.Extra
 import Control.Exception.Extra
+import System.Environment.Extra
+import System.Exit
 
 main = do
-    retry 3 $ system_ "cabal install cereal json binarydefer data-lens QuickCheck --force-reinstalls"
+    ver <- lookupEnv "GHCVER"
+    (\act -> if ver == Just "head" then catch_ act $ const exitSuccess else act) $
+        retry 3 $ system_ "cabal install cereal json binarydefer data-lens QuickCheck --force-reinstalls"
     system_ "runhaskell Main --generate"
     system_ "git diff --exit-code"
     system_ "runhaskell Main --test"
