01777a5d0f9361ae012e0c266ea9c9ad5ed572b0
Neil Mitchell <ndmitchell@gmail.com>
2017-08-01 15:32:44 +0100

    Make sure you don't install derive from the repo, but from HEAD

--- travis.hs
+++ travis.hs
@@ -9,7 +9,7 @@ main = do
     (\act -> if ver == Just "head" then catch_ act $ const exitSuccess else act) $
         retry 3 $ system_ "cabal install cereal json binarydefer QuickCheck --force-reinstalls"
     -- installing other stuff might break derive, so force that it gets rebuilt
-    retry 3 $ system_ "cabal install derive --force-reinstalls"
+    retry 3 $ system_ "cabal install --force-reinstalls"
     system_ "runhaskell Main --generate"
     system_ "git diff --exit-code"
     system_ "runhaskell Main --test"
